939247e7653c3bf81f331848c66dfc33e0850dc1
Cody Fritz <clanalia55@gmail.com>
2020-07-31 16:34:36 -0700

    fix spelling errors, proceeding with debugging

--- Claires/Controllers/ClientsController.cs
+++ Claires/Controllers/ClientsController.cs
@@ -16,7 +16,7 @@ namespace Claires.Controllers
     }
     public ActionResult Details(int id)
     {
-      Client thisClient = _db.Clients.FirstOrDefault(donations => donations.DonationId == id);
+      Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
       return View(thisClient);
     }
     public ActionResult Create()
@@ -51,7 +51,7 @@ namespace Claires.Controllers
       return RedirectToAction("Index");
     }
     [HttpPost]
-    public ActionResult Create(Client client1)
+    public ActionResult Create(Client client)
     {
       _db.Clients.Add(client);
       _db.SaveChanges();
--- Claires/Models/ClairesContext.cs
+++ Claires/Models/ClairesContext.cs
@@ -5,7 +5,8 @@ namespace Claires.Models
   public class ClairesContext : DbContext
   {
     public virtual DbSet<Stylist> Stylists { get; set; }
-    public DbSet<Client> Clients { get;set;}
-    public ClairsContext(DbContextOptions options) : base(options) {}
+    public DbSet<Client> Clients { get; set; }
+
+    public ClairesContext(DbContextOptions options) : base(options) { }
   }
 }
